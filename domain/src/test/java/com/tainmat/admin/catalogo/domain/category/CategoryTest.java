e9ed52d8dded0ddb7a3a5c5bfcd6b2ee79e317e1
Tainã Pino <taina.pino@casadoconstrutor.com.br>
2025-02-05 06:54:31 -0300

    feat: implementa a classe CategoryID

--- domain/src/test/java/com/tainmat/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/tainmat/admin/catalogo/domain/category/CategoryTest.java
@@ -9,19 +9,35 @@ public class CategoryTest {
     public void givenAValidParams_whenCallNewCategory_ThenInstantiateACategory() {
         final var expectedName = "filmes";
         final var expectedDescription = "A categoria mais assistidas";
-        final var expectesIsActive = true;
-        Category.newCategory(expectedName, expectedDescription, expectesIsActive);
+        final var expectedIsActive = true;
+        Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
-        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectesIsActive);
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
         Assertions.assertNotNull(actualCategory);
         Assertions.assertNotNull(actualCategory.getId());
         Assertions.assertEquals(expectedName, actualCategory.getName());
         Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
-        Assertions.assertEquals(expectesIsActive, actualCategory.isActive());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
         Assertions.assertNotNull(actualCategory.getCreatedAt());
         Assertions.assertNotNull(actualCategory.getUpdatedAt());
         Assertions.assertNull(actualCategory.getDeletedAt());
 
+    }
+
+    @Test
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenThrowException() {
+        final String expectedName = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedDescription = "A categoria mais assistidas";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate());
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).getMessage());
     }
 }
